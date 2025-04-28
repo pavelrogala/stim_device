95338a8d2a6552e6acb9d7d942e8679c7dcee2d0
Pavel Rogala <pavel.rogala@gmail.com>
2025-04-28 17:06:24 +0200

    Error sound now plays after the revive counter hits 5

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@ const int ledBarPins[10] = {3, 4, 5, 6, 7, 10, 11, 12, A0, A1}; // 10 LEDs
 
 // Variables
 int counter = 0;
+bool errorState = false;
 
 void setup() {
   // Initialize pins
@@ -33,6 +34,14 @@ void updateLedBar(int value) {
   }
 }
 
+void playErrorTone() {
+  tone(buzzerPin, 500);
+  delay(150);
+  tone(buzzerPin, 300);
+  delay(150);
+  noTone(buzzerPin);
+}
+
 void loop() {
   static unsigned long buttonPressStart = 0;
   static unsigned long lastButtonChange = 0;
@@ -40,21 +49,38 @@ void loop() {
   static bool secondLedOn = false;
   static bool lastButtonState = HIGH;
   static bool sweepingTone = false;
+  static bool countedThisPress = false;
 
   bool currentButtonState = digitalRead(buttonPin);
 
+  // Debounce
   if (currentButtonState != lastButtonState) {
     lastButtonChange = millis();
     lastButtonState = currentButtonState;
   }
 
   if ((millis() - lastButtonChange) > 50) { // Debounce
+    if (errorState) {
+      if (currentButtonState == LOW && !counting) {
+        // Button just pressed -> play error tone immediately
+        playErrorTone();
+        counting = true;
+      } else if (currentButtonState == HIGH && counting) {
+        // Button released -> ready to detect next press
+        counting = false;
+      }
+      return; // Skip all normal logic when in error state
+    }
+    
+
     if (currentButtonState == LOW) {
+      // Button pressed
       if (!counting) {
         buttonPressStart = millis();
         counting = true;
         secondLedOn = false;
-        sweepingTone = true; // Start sweeping when button is first pressed
+        sweepingTone = true;
+        countedThisPress = false;
       }
 
       unsigned long heldTime = millis() - buttonPressStart;
@@ -63,24 +89,20 @@ void loop() {
         digitalWrite(ledPin1, HIGH);
         digitalWrite(ledPin2, LOW);
 
-        // Sweep tone between 300Hz and 1000Hz over 2 seconds
         if (sweepingTone) {
-          int sweepFrequency = 300 + (700.0 * heldTime / 2000); // Linear interpolation
+          int sweepFrequency = 300 + (700.0 * heldTime / 2000);
           tone(buzzerPin, sweepFrequency);
         }
       } else {
-        if (!secondLedOn) {
-          // Stop sweeping
+        if (!secondLedOn && !countedThisPress) {
           noTone(buzzerPin);
           sweepingTone = false;
 
-          // Increment counter
           if (counter < 5) {
             counter++;
+            Serial.print("Counter: ");
+            Serial.println(counter);
           }
-          Serial.print("Counter: ");
-          Serial.println(counter);
-          secondLedOn = true;
 
           // Confirmation 3-beep
           for (int i = 0; i < 3; i++) {
@@ -89,11 +111,21 @@ void loop() {
             noTone(buzzerPin);
             delay(100);
           }
+
+          secondLedOn = true;
+          countedThisPress = true;
         }
+
         digitalWrite(ledPin1, LOW);
         digitalWrite(ledPin2, HIGH);
       }
     } else {
+      // Button released
+      if (countedThisPress && counter >= 5) {
+        errorState = true; // Enter errorState after releasing 5th time
+        Serial.println("Entering error state!");
+      }
+
       digitalWrite(ledPin1, LOW);
       digitalWrite(ledPin2, LOW);
       noTone(buzzerPin);
@@ -103,5 +135,8 @@ void loop() {
     }
   }
 
-  updateLedBar(counter);
+  // Update LEDs only if not in error state
+  if (!errorState) {
+    updateLedBar(counter);
+  }
 }
